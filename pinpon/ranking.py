b7bc3dca15346b5c9952147ee0209e0593a01d60
Facundo Olano <facundo.olano@gmail.com>
2019-01-21 00:11:54 -0300

    implement ranking algorithm

--- pinpon/ranking.py
+++ pinpon/ranking.py
@@ -1,12 +1,47 @@
+import pinpon.models as models
+
 def current():
     "Return the latest version of the player ranking"
-    # FIXME implement
-    pass
+    # all players start with 10 points
+    players = models.Player.objects.all()
+    counter = {p: 10 for p in players}
 
-def rank(ranking, user):
+    # TODO should be the default order
+    matches = models.Match.objects.order_by('date')
+    for match in matches:
+        winner = match.winner()
+        if winner:
+            loser = match.player2 if winner is match.player1 else match.player1
+            counter[winner] += counter[loser] // 2
+
+    mapping = points2rank(counter)
+    ranking = {}
+    for player, points in counter.items():
+        ranking[player] = {"rank": mapping[points], "points": points}
+
+    return ranking
+
+
+def rank(ranking, player):
     """
     Return the current ranking and points of the given user.
     Users with the same amount of points will have the same ranking.
     """
-    # FIXME implement
-    return 1, 0
+    rank = ranking[player]
+    return rank["rank"], rank["points"]
+
+## helper functions
+
+def points2rank(counter):
+    """
+    Given a point counter dict return a mapping from points to rank index,
+    with shared indexes for ties.
+    """
+    points = sorted(counter.values(), reverse=True)
+    mapping = {}
+    rank = 1
+    for p in points:
+        if p not in mapping:
+            mapping[p] = rank
+        rank += 1
+    return mapping
